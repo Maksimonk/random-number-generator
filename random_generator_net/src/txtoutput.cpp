00ace3e30b433aa5d03410e911ea9838f89d3551
GlebKasi <glebk-vrn@yandex.ru>
2024-12-03 21:55:07 +0300

    Побитовый вывод в файл

--- random_generator_net/src/txtoutput.cpp
+++ random_generator_net/src/txtoutput.cpp
@@ -23,7 +23,16 @@ int main(int argc, char **argv)
 	
 	ff = fopen("numbers.txt","w");
 	for(int i = 0; i < PULL; i++){
-		fprintf(ff, "%llu\n", data[i]);
+		for(int j = 0; j < SIZE; j++){
+			for (int k = 0; k < 8; k++)
+			{
+				if (data[i][j] & 0x80)
+					fprintf(ff, "1");
+				else
+					fprintf(ff, "0");
+				data[i][j] = data[i][j] << 1;
+			}
+		}
 	}
 	fclose(ff);
 	return 0;
